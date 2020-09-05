b01403c6094b359ba416bd2f00f5c610f3f7e496
Matt Carpenter <mattcarpenter@gmail.com>
2020-09-05 15:48:33 +0900

    fix newline replacement regex pattern

--- src/components/Honbun.tsx
+++ src/components/Honbun.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import AnnotatedGrammar from './AnnotatedGrammar';
 import reactStringReplace from 'react-string-replace';
-import ScaleText from "react-scale-text";
+
+const styles = {
+  lineHeight: '30px'
+};
 
 const replaceGrammarPattern = /(\([\s\S][^()[\]]*?\)\[.+\])/g;
 
 function Honbun({ honbun, grammar }) {
-
-  // try my own replacement
-  const text = honbun.text;
-  let m = replaceGrammarPattern.exec(text);
-  debugger;
 
   // prepare grammar annotations
   let replacedText = reactStringReplace(honbun.text, replaceGrammarPattern, (match, i) => {
@@ -19,21 +17,21 @@ function Honbun({ honbun, grammar }) {
     const annotationMatch = annotationMatchPattern.exec(match);
 
     if (!annotationMatch) {
-      return <span>BAD</span>
+      return <span>BADBADBDDDDDDDDDDDDDD</span>
     }
 
     return <AnnotatedGrammar text={annotationMatch[1]} grammar={grammar[annotationMatch[2]]} />
   });
 
   // prepare linebreaks
-  replacedText = reactStringReplace(replacedText, /\n/g, (match, i) =>
+  replacedText = reactStringReplace(replacedText, /(\n)/g, (match, i) =>
     <br />
   );
 
   return (
-    <ScaleText widthOnly={true}>
+    <div style={styles}>
       {replacedText}
-    </ScaleText>
+    </div>
   )
 }
 
